c96b5859798b2e9115e8b5e563287629f5589b99
ManuelFalchettoni <manufalchettoni@gmail.com>
2026-01-28 23:52:47 -0300

    refactor: migrate project to Maven and adopt standard directory structure

--- src/main/java/com/falchettoni/linguist/Main.java
+++ src/main/java/com/falchettoni/linguist/Main.java
@@ -4,9 +4,9 @@ import com.falchettoni.linguist.repository.VocabularyRepository;
 import com.falchettoni.linguist.service.FileService;
 import com.falchettoni.linguist.ui.ConsoleUI;
 
-class Main { // Application entry point
-    static void main() {// Main method
-        VocabularyRepository repo = new FileService("vocabulary.csv"); // Initialize repository with file service
+public class Main { // Application entry point
+    public static void main(String[] args) {// Main method
+        VocabularyRepository repo = new FileService("src/main/resources/vocabulary.csv"); // Initialize repository with file service
         var app = new ConsoleUI(repo); // Create an instance of ConsoleUI
         app.start(); // Start the console user interface
     }
